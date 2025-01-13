b63f4c4fd9d3a92aa1e1c9ca9c194bd45929e4a7
DJPaws <dan.mitchell634@gmail.com>
2025-01-12 21:18:44 -0500

    added clock to track dt

--- main.py
+++ main.py
@@ -9,13 +9,20 @@ def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	
+	my_clock = pygame.time.Clock() 			# initialize clock to track FPS
+	dt = 0 									# delta time
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill((0, 0, 0))
-		pygame.display.flip()
+		screen.fill((0, 0, 0))						# create black screen
+		pygame.display.flip()						# refresh screen during loop
+		elapsed_ms = my_clock.tick(60) 				# limit to 60 FPS and returns milliseconds passed
+		dt = elapsed_ms / 1000 						# convert to seconds and stores as delta time
 
 if __name__ == "__main__":
 	main()
