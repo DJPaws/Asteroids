90eb8ee07a1053510fe1a4be3a8d9471acd04626
DJPaws <dan.mitchell634@gmail.com>
2025-01-13 18:08:35 -0500

    updated with player function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -11,7 +12,6 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	
 	my_clock = pygame.time.Clock() 			# initialize clock to track FPS
 	dt = 0 									# delta time
 
@@ -20,9 +20,13 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0, 0, 0))						# create black screen
+
+		player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+		player.draw(screen)
 		pygame.display.flip()						# refresh screen during loop
+
 		elapsed_ms = my_clock.tick(60) 				# limit to 60 FPS and returns milliseconds passed
-		dt = elapsed_ms / 1000 						# convert to seconds and stores as delta time
+		dt = elapsed_ms / 1000						# convert to seconds and stores as delta time
 
 if __name__ == "__main__":
 	main()
